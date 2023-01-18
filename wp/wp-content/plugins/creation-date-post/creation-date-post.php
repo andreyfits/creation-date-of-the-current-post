e10d0227172ace4e287bc79126befc32d4e5ce00
Andrey Fits <andreyfits@gmail.com>
2023-01-18 23:11:19 +0300

    feat: add basic plugin information

---
+++ wp/wp-content/plugins/creation-date-post/creation-date-post.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Creation date post
+ *
+ * @package           CreationDatePostPackage
+ * @author            Andrei Fits
+ * @copyright         2023 Andrei Fits
+ * @license           GPL-2.0-or-later
+ *
+ * @creation-date-post
+ * Plugin Name:       The creation date of the current post
+ * Plugin URI:        https://example.com/plugin-name
+ * Description:       This plugin adding the creation date of the current post in the format that is specified in the WordPress settings to the title of each blog post on the site.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.4+
+ * Author:            Andrei FIts
+ * Author URI:        https://example.com
+ * Text Domain:       creation-date-post
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://example.com/my-plugin/
+ */
